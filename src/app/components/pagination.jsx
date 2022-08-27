bb88b5edf8b67733d50e9f560cd73e6f0dc485a4
Ivan Petrov <fatevector@yandex.ru>
2022-08-27 19:07:20 +0300

    render pagination elements

--- src/app/components/pagination.jsx
+++ src/app/components/pagination.jsx
@@ -1,5 +1,23 @@
+import _ from "lodash";
+
 const Pagination = (props) => {
-    return <div onClick={props.onPageChange(1)}> Click </div>;
+    const { itemsCount, pageSize, onPageChange } = props;
+    const pageCount = Math.ceil(itemsCount / pageSize);
+    if (pageCount === 1) return null;
+    const pages = _.range(1, pageCount + 1);
+    return (
+        <nav>
+            <ul className="pagination">
+                {pages.map((page) => (
+                    <li className="page-item" key={"page_" + page}>
+                        <a href="#" className="page-link">
+                            {page}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
 };
 
 export default Pagination;
