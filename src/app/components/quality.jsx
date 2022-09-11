a40a77bc6b820b30b762a64f9c08de2072ae6458
Ivan Petrov <fatevector@yandex.ru>
2022-09-12 00:56:39 +0300

    delete _id from props in Quality component

--- src/app/components/quality.jsx
+++ src/app/components/quality.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Quality = ({ _id, color, name }) => {
+const Quality = ({ color, name }) => {
     return <span className={`badge bg-${color} m-1`}>{name}</span>;
 };
 
 Quality.propTypes = {
-    _id: PropTypes.string.isRequired,
     color: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired
 };
