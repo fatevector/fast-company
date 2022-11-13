35d07d6fe339b7415c925e9ba07e1d4240d1f87b
Ivan Petrov <fatevector@yandex.ru>
2022-11-13 22:13:01 +0300

    implementing of main navigation

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Users from "./layouts/users";
+import Main from "./layouts/main";
+import Login from "./layouts/login";
 import NavBar from "./components/navBar";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 const App = () => {
     return (
         <>
             <NavBar />
-            <Users />
+            <Switch>
+                <Route exact path="/" component={Main} />
+                <Route path="/login" component={Login} />
+                <Route path="/users" component={Users} />
+                <Redirect to="/" />
+            </Switch>
         </>
     );
 };
