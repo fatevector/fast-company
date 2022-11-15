ae4a65235b7a5b9756ef338b922d98a175a47210
Ivan Petrov <fatevector@yandex.ru>
2022-11-15 18:43:01 +0300

    init form for login

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Login = () => {
-    return <h1>Login</h1>;
+    return (
+        <form action="">
+            <div>
+                <lavel htmlFor="email">Email</lavel>
+                <input type="text" id="email" />
+            </div>
+            <div>
+                <lavel htmlFor="password">Пароль</lavel>
+                <input type="password" id="password" />
+            </div>
+        </form>
+    );
 };
 
 export default Login;
