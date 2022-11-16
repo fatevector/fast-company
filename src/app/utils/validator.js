3cf1953d333cda2a976cf0ffe855fe0e6573cd18
Ivan Petrov <fatevector@yandex.ru>
2022-11-16 18:39:24 +0300

    adding 'isCapitalSymbol', 'isDigit' and 'minLength' validators

--- src/app/utils/validator.js
+++ src/app/utils/validator.js
@@ -1,18 +1,35 @@
 export const validator = (data, config) => {
     const errors = {};
     const validate = (validateMethod, data, config) => {
+        let validationFailed = false;
         switch (validateMethod) {
-            case "isRequired":
-                if (data.trim() === "") return config.message;
+            case "isRequired": {
+                validationFailed = data.trim() === "";
                 break;
+            }
             case "isEmail": {
                 const emailRegExp = /^\S+@\S+\.\S+$/g;
-                if (!emailRegExp.test(data)) return config.message;
+                validationFailed = !emailRegExp.test(data);
+                break;
+            }
+            case "isCapitalSymbol": {
+                const capitalRegExp = /[A-Z]+/g;
+                validationFailed = !capitalRegExp.test(data);
+                break;
+            }
+            case "isDigit": {
+                const digitRegExp = /\d+/g;
+                validationFailed = !digitRegExp.test(data);
+                break;
+            }
+            case "minLength": {
+                validationFailed = data.length < config.value;
                 break;
             }
             default:
                 break;
         }
+        if (validationFailed) return config.message;
     };
     for (const fieldName in data) {
         for (const validateMethod in config[fieldName]) {
