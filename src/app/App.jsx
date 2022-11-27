35abeaa53638348d54280fbee65fb7e9804b5ba8
Ivan Petrov <fatevector@yandex.ru>
2022-11-27 23:01:47 +0300

    add profession.service.js, useProfession hook and Profession component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import NavBar from "./components/ui/navBar";
 import Main from "./layouts/main";
 import Login from "./layouts/login";
 import Users from "./layouts/users";
+import { ProfessionProvider } from "./hooks/useProfession";
 
 const App = () => {
     return (
         <>
             <NavBar />
-            <Switch>
-                <Route exact path="/" component={Main} />
-                <Route path="/login/:type?" component={Login} />
-                <Route path="/users/:userId?/:editingMode?" component={Users} />
-                <Redirect to="/" />
-            </Switch>
+            <ProfessionProvider>
+                <Switch>
+                    <Route exact path="/" component={Main} />
+                    <Route path="/login/:type?" component={Login} />
+                    <Route
+                        path="/users/:userId?/:editingMode?"
+                        component={Users}
+                    />
+                    <Redirect to="/" />
+                </Switch>
+            </ProfessionProvider>
+            <ToastContainer />
         </>
     );
 };
