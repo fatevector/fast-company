03034fa95dd512e7c9a68bd791fbc11b297194a6
Иван Петров <fatevector@yandex.ru>
2023-02-10 15:19:53 +0300

    add qualitiesReducer

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
@@ -11,8 +11,14 @@ import { QualityProvider } from "./hooks/useQuality";
 import AuthProvider from "./hooks/useAuth";
 import ProtectedRoute from "./components/common/protectedRoute";
 import LogOut from "./layouts/logOut";
+import { useDispatch } from "react-redux";
+import { loadQualitiesList } from "./store/qualities";
 
 const App = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(loadQualitiesList());
+    }, []);
     return (
         <>
             <AuthProvider>
