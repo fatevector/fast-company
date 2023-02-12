ee8d3a1c20f06b839cbeb30a70bb268fc17eb414
Иван Петров <fatevector@yandex.ru>
2023-02-12 21:56:02 +0300

    remove all hooks

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,7 +6,6 @@ import NavBar from "./components/ui/navBar";
 import Main from "./layouts/main";
 import Login from "./layouts/login";
 import Users from "./layouts/users";
-import AuthProvider from "./hooks/useAuth";
 import ProtectedRoute from "./components/common/protectedRoute";
 import LogOut from "./layouts/logOut";
 import AppLoader from "./components/ui/hoc/appLoader";
@@ -15,19 +14,17 @@ const App = () => {
     return (
         <>
             <AppLoader>
-                <AuthProvider>
-                    <NavBar />
-                    <Switch>
-                        <Route exact path="/" component={Main} />
-                        <Route path="/login/:type?" component={Login} />
-                        <ProtectedRoute
-                            path="/users/:userId?/:editingMode?"
-                            component={Users}
-                        />
-                        <Route path="/logout" component={LogOut} />
-                        <Redirect to="/" />
-                    </Switch>
-                </AuthProvider>
+                <NavBar />
+                <Switch>
+                    <Route exact path="/" component={Main} />
+                    <Route path="/login/:type?" component={Login} />
+                    <ProtectedRoute
+                        path="/users/:userId?/:editingMode?"
+                        component={Users}
+                    />
+                    <Route path="/logout" component={LogOut} />
+                    <Redirect to="/" />
+                </Switch>
                 <ToastContainer />
             </AppLoader>
         </>
