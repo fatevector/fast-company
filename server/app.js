e042ee9867b5045ffff18a13105b9d032e1e369b
Иван Петров <fatevector@yandex.ru>
2023-02-18 13:47:42 +0300

    add middleware for express

--- server/app.js
+++ server/app.js
@@ -5,13 +5,16 @@ const chalk = require("chalk");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const PORT = config.get("port") ?? 8080;
 
-if (process.env.NODE_ENV === "production") {
-    console.log("Production");
-} else {
-    console.log("Development");
-}
+// if (process.env.NODE_ENV === "production") {
+//     console.log("Production");
+// } else {
+//     console.log("Development");
+// }
 
 app.listen(PORT, () => {
     console.log(chalk.green(`Server has been started on port ${PORT}...`));
