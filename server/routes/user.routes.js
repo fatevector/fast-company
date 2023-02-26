e239a6362a3b78c491f1f9faf6548ba7931217e5
Иван Петров <fatevector@yandex.ru>
2023-02-26 18:41:39 +0300

    add routes for getting a users list and for user updating

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,4 +1,41 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router({ mergeParams: true });
+
+router.patch("/:userId", async (req, res) => {
+    try {
+        const { userId } = req.params;
+
+        // todo: userId === current user id
+        if (userId) {
+            const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+                new: true
+            });
+            res.send(updatedUser);
+        } else {
+            res.status(401).json({
+                error: {
+                    message: "UNAUTHORIZED",
+                    code: 401
+                }
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "На сервере произошла ошибка. Попробуйте позже"
+        });
+    }
+});
+
+router.get("/", async (req, res) => {
+    try {
+        const list = await User.find();
+        res.status(200).send(list);
+    } catch (error) {
+        res.status(500).json({
+            message: "На сервере произошла ошибка. Попробуйте позже"
+        });
+    }
+});
 
 module.exports = router;
